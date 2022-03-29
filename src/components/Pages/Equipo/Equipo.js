2faed5c7c6a28064bcd201fe2789051e18ca880f
Juan Manuel Narvaja <78772973+PupiNarvaja@users.noreply.github.com>
2022-03-29 19:33:17 -0300

    Update Equipo.js

--- src/components/Pages/Equipo/Equipo.js
+++ src/components/Pages/Equipo/Equipo.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import title from '../../../assets/images/title-equipo.png';
 
 const Equipo = () => {
   return (
     <>
         <div className='home'>
-            <div className='container'>
-                <img src={title} alt='Main title' className='home--title'></img>
-            </div>
+          
         </div>
     </>
   )
