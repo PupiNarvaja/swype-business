5056c42620aed420e87d3a0d54032eb7ec72b6dc
Juan Manuel Narvaja <78772973+PupiNarvaja@users.noreply.github.com>
2022-03-29 19:32:51 -0300

    Update Contacto.js

--- src/components/Pages/Contacto/Contacto.js
+++ src/components/Pages/Contacto/Contacto.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import title from '../../../assets/images/title-contacto.png';
 
 const Contacto = () => {
   return (
     <>
         <div className='home'>
-            <div className='container'>
-                <img src={title} alt='Main title' className='home--title'></img>
-            </div>
+          
         </div>
     </>
   )
