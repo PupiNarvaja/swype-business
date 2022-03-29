cc7ff1daf7cfcfefdb5efceb4bf3edf40fd57da6
Juan Manuel Narvaja <78772973+PupiNarvaja@users.noreply.github.com>
2022-03-29 19:34:24 -0300

    Update Estudio.js

--- src/components/Pages/Estudio/Estudio.js
+++ src/components/Pages/Estudio/Estudio.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import logo from '../../../assets/images/logo.png';
-import title from '../../../assets/images/title-estudio.png';
 import './Estudio.css';
 
 const Estudio = () => {
   return (
       <>
-        <div className='home'>
-            <div className='container'>
-                <img src={title} alt='Main title' className='home--title'></img>
-            </div>
-        </div>
         <h2 className='title'>¿Quiénes somos?</h2>
         <h2 className='title'>¿Cómo trabajamos?</h2>
         <h2 className='title'>¿Cómo nace 
